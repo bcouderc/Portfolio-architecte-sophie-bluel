4e47887ef1060ca954954ed64151654d18900580
Béatrice COUDERC <137801013+bcouderc@users.noreply.github.com>
2024-01-19 15:39:11 +0100

    retour état précédent createModalGallery

--- FrontEnd/modal.js
+++ FrontEnd/modal.js
@@ -233,32 +233,30 @@ function modalGaleriePhoto() {
     // Ajout des photos dans la galerie
     function createModalGallery(work) {
         for (let i = 0; i < work.length; i++) {
-            ajoutPhoto(work[i]);
+            const newFig = document.createElement('figure');
+            const imgGallery = document.createElement('img');
+
+
+            imgGallery.setAttribute('src', work[i].imageUrl);
+            newFig.classList.add('crash');
+            newFig.id = 'vignette' + work[i].id;
+            const corbeille = document.createElement('i');
+            corbeille.classList.add('crash2', 'fa-solid', 'fa-trash-can');
+
+            // Ajout d'un gestionnaire d'événements au clic de l'image
+            corbeille.addEventListener('click', () => {
+                // Appel de la fonction pour gérer le clic avec l'ID de l'image
+                deleteModalGalery(work[i].id);
+            });
+
+            newFig.appendChild(corbeille);
+            newDivGalerie.appendChild(newFig);
+            newFig.appendChild(imgGallery);
         }
     }
     updateModalGallery();
 }
 
-function ajoutPhoto(photoItem){
-    const newFig = document.createElement('figure');
-    const imgGallery = document.createElement('img');
-
-    imgGallery.setAttribute('src', photoItem.imageUrl);
-    newFig.classList.add('crash');
-    newFig.id = 'vignette' + photoItem.id;
-    const corbeille = document.createElement('i');
-    corbeille.classList.add('crash2', 'fa-solid', 'fa-trash-can');
-
-    // Ajout d'un gestionnaire d'événements au clic de l'image
-    corbeille.addEventListener('click', () => {
-        // Appel de la fonction pour gérer le clic avec l'ID de l'image
-        deleteModalGalery(photoItem.id);
-    });
-
-    newFig.appendChild(corbeille);
-    newDivGalerie.appendChild(newFig);
-    newFig.appendChild(imgGallery);
-}
 
 // *****************************************************************************
 // GESTION SUPPRESSION DE LA GALLERY PHOTO
